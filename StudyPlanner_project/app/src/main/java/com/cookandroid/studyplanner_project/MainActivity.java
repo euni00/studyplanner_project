fc4bcf8ac3e00be054219214fb844e133da64558
euni00 <euneee2000@naver.com>
2022-11-18 09:08:25 +0900

    Update main code

--- StudyPlanner_project/app/src/main/java/com/cookandroid/studyplanner_project/MainActivity.java
+++ StudyPlanner_project/app/src/main/java/com/cookandroid/studyplanner_project/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_timer);
+        // setContentView(R.layout.activity_stopwatch);
 
     }
 }
